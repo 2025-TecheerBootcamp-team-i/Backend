447a2f9c1dc85e490fe5f29158a7484b583b8bb9
jaewon <dlwodbjs2003@gmail.com>
2026-01-13 02:05:49 +0900

    docx:주석 추가

--- music/serializers.py
+++ music/serializers.py
@@ -1,8 +1,14 @@
+"""
+인증 관련 Serializer
+사용자 회원가입 및 로그인 데이터 검증 및 변환
+"""
 from rest_framework import serializers
 from django.contrib.auth.hashers import make_password
 from .models import Users
 
+
 class UserRegisterSerializer(serializers.ModelSerializer):
+    """회원가입 Serializer - 이메일, 비밀번호, 닉네임 입력받아 사용자 생성"""
     password = serializers.CharField(write_only=True, min_length=4)
     
     class Meta:
@@ -10,10 +16,12 @@ class UserRegisterSerializer(serializers.ModelSerializer):
         fields = ['email', 'password', 'nickname']
     
     def create(self, validated_data):
-        # 비밀번호 해싱
+        """비밀번호 해싱 후 사용자 생성"""
         validated_data['password'] = make_password(validated_data['password'])
         return Users.objects.create(**validated_data)
 
+
 class UserLoginSerializer(serializers.Serializer):
+    """로그인 Serializer - 이메일, 비밀번호 입력 검증"""
     email = serializers.EmailField()
     password = serializers.CharField(write_only=True)
