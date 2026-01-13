5e838a00c51920368c28ffec7a32829cb79603da
hhyunseung1216 <goodboy1216@naver.com>
2026-01-13 14:32:55 +0900

    feat:회원가입 시 users DB 에러해결

--- music/serializers.py
+++ music/serializers.py
@@ -4,6 +4,7 @@
 """
 from rest_framework import serializers
 from django.contrib.auth.hashers import make_password
+from django.utils import timezone  # 타임스탬프 자동 설정을 위해 추가
 from .models import Users
 
 
@@ -17,7 +18,16 @@ class UserRegisterSerializer(serializers.ModelSerializer):
     
     def create(self, validated_data):
         """비밀번호 해싱 후 사용자 생성"""
+        # 비밀번호를 해시값으로 변환 (보안)
         validated_data['password'] = make_password(validated_data['password'])
+        
+        # 타임스탬프 및 삭제 플래그 자동 설정
+        # Users 모델이 managed=False로 설정되어 있어 auto_now_add/auto_now가 작동하지 않음
+        # 따라서 수동으로 값을 설정해야 함
+        validated_data['created_at'] = timezone.now()  # 생성 시간
+        validated_data['updated_at'] = timezone.now()  # 수정 시간 (생성 시에는 created_at과 동일)
+        validated_data['is_deleted'] = False  # 삭제 플래그 (기본값: False)
+        
         return Users.objects.create(**validated_data)
 
 
